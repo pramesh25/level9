f5823b285211a7dc6fcb4638b8e3fbfb5b466897
Pramesh Dhaugoda <prameshdhaugoda23@gmail.com>
2020-10-22 11:50:42 +0545

    update

--- Project1.py
+++ Project1.py
@@ -1,76 +1,72 @@
 import csv
+import pandas as pd
+import requests
 import bs4
-import urllib
 from bs4 import BeautifulSoup as soup
-import urllib.parse
 from urllib.request import urlopen as uReq
+import urllib.parse
+def site(iata_code):
+    my_url = 'https://en.wikipedia.org/wiki/' + iata_code + '_Airport'
+    uClient = requests.get(my_url)
+    return uClient
 
 
-url = 'https://en.wikipedia.org/wiki/'
-with open('airportcode_1.csv' , 'r') as read_file:
-    reader = csv.reader(read_file)
-    row = list(reader)
-    code = []
-    for i in range(1,len(row)-1):
-        code.append(row[i][0])
 
 
-data = {}
-data['airportcode'] = code[0]
-print(data)
-url_values = urllib.parse.urlencode(data)
-print(url_values)
-full_url = url + '?' + url_values
-try: data = uReq(full_url)
-except urllib.error.URLError as e:
-    print(e.reason)
-print(data)
-# while repeat == True :
-#
-#
-#     print(data)
-#     r = input('Do you want to repeat again')
-#     r = r.lower()
-#     if r == 'y' or r == 'Y':
-#         repeat = True
-#         i = i +1
-#     else:
-#         r = False
 
-# values
-#
-my_url = 'https://en.wikipedia.org/wiki/Keflav%C3%ADk_International_Airport'
-def scrap():
-    uClient = uReq(my_url)
-    page_html = uClient.read()
-    uClient.close()
-    page_soup = soup(page_html , "html.parser") #html parsing
-    containers = page_soup.findAll("table", {"class" : "infobox vcard"})
-    container = containers[0]
-    code = container.findAll("div",{"class" : "hlist hlist-separated"})
-    code = code[0]
-    code = code.findAll("span", {"class": "nickname"})
-    iata = code[0].text
-    icao = code[1].text
-    tr = container.findAll("tr")
-    location = tr[8].td.text.split(",")
-    airport_name = container.tr.th.div.text
-    find_location = container.findAll("td" , {"class" : "label"})
-    location= find_location[0].text
-    gps = container.find('span', {"class": "geo-dec"}).text
-    l = [iata,icao,airport_name,location,gps]
-    return l
 
-def add_text():
-    with open('airport.csv','a') as file:
+def Scrap():
+    with open('airportcode_1.csv','r') as read_file:
+        reader = csv.reader(read_file)
+        next(reader)
+        row = list(reader)
+        for i in row:
+            iata_code = i[0]
+            print(iata_code)
+            respond = site(iata_code).url
+            source = requests.get(respond)
+            s = soup(source.text, 'lxml')
+            try:
+                table = s.findAll("table", {"class": "infobox vcard"})
+                table = table[0]
+            except:
+                print('not found')
+
+
+            try:
+                code = table.findAll("div", {"class": "hlist hlist-separated"})
+                code = code[0]
+                code = code.findAll("span", {"class": "nickname"})
+                iata = code[0].text
+                icao = code[1].text
+            except:
+                print('error in iata or icao')
+                iata = 'none'
+                icao = 'none'
+            try:
+                airport_name = table.tr.th.div.text
+            except:
+                print('error on airport name')
+                airport_name = 'none'
+            try:
+                tr = table.findAll("tr")
+                location = tr[8].td.text.split(",")
+            except:
+                print('error on location')
+                location = 'none'
+            # find_location = container.findAll("td" , {"class" : "label"})
+            # location= find_location[0].text
+            try:
+                gps = table.find('span', {"class": "geo-dec"}).text
+            except:
+                print('error on gps')
+                gps = 'none'
+            l = [iata, icao, airport_name, location, gps]
+            print(l)
+with open('airport.csv', 'w') as file:
         writer = csv.writer(file)
-        writer.writerow(scrap())
-
-
-
-add_text()
-
-
+        writer.writerow(['IATA', 'IACO', 'Airport name', 'Location', 'Gps'])
+        writer.writerow(Scrap())
 
 
 
--- project.py
+++ project.py
@@ -4,26 +4,44 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 
-my_url = input('enter url :')
+with open('airport.csv' , 'w') as file1:
+    head = ['IATA' , 'ICAO' , 'Airport Name' , 'Location' , 'Gps']
+    writer = csv.writer(file1)
+
+my_url = 'https://en.wikipedia.org/wiki/Jacksons_International_Airport'
+
+
 uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
 page_soup = soup(page_html , "html.parser") #html parsing
 containers = page_soup.findAll("table", {"class" : "infobox vcard"})
 container = containers[0]
-code = container.findAll("div",{"class" : "hlist hlist-separated"})
-code = code[0]
-code = code.findAll("span", {"class": "nickname"})
-iata = code[0].text
-icao = code[1].text
-airport_name = container.tr.th.div.text
-tr = container.findAll("tr")
-location = tr[8].td.text.split(",")
+try:
+    code = container.findAll("div",{"class" : "hlist hlist-separated"})
+    code = code[0]
+    code = code.findAll("span", {"class": "nickname"})
+    iata = code[0].text
+    icao = code[1].text
+except:
+    print('error')
+try :
+    airport_name = container.tr.th.div.text
+except:
+    print('error on airport name')
+try :
+    tr = container.findAll("tr")
+    location = tr[8].td.text.split(",")
+except:
+    print('error on location')
 # find_location = container.findAll("td" , {"class" : "label"})
 # location= find_location[0].text
-gps = container.find('span', {"class": "geo-dec"}).text
+try:
+    gps = container.find('span', {"class": "geo-dec"}).text
+except:
+    print('error on gps')
 l = [iata,icao,airport_name,location,gps]
 print(l)
-with open('airport.csv','a') as file:
+with open('airport.csv','w') as file:
     writer = csv.writer(file)
     writer.writerow(l)
